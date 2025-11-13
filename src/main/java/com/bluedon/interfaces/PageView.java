aa5f537dcd9abf58ba3017681f0924d389b2d4af
up-jacky <149249055+up-jacky@users.noreply.github.com>
2025-11-13 14:42:11 +0800

    docs: add documentation for the class under the package com.bluedon.interfaces

--- src/main/java/com/bluedon/interfaces/PageView.java
+++ src/main/java/com/bluedon/interfaces/PageView.java
@@ -2,10 +2,20 @@ package com.bluedon.interfaces;
 
 import javafx.stage.Stage;
 
+/**
+ * Main interface for what methods of each page should have.
+ */
 public interface PageView {
 	
+	/**
+	 * Displays the stage to the application.
+	 * @param primaryStage
+	 */
 	public void displayPage(Stage primaryStage);
 
+	/**
+	 * Initializes the pages.
+	 */
 	public void init();
 
 	
